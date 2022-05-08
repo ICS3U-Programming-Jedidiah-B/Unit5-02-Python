776cbec5b1120a11b5c1ea2e8222252cb8c87ae4
Ubuntu <ubuntu@ip-172-31-74-87.ec2.internal>
2022-05-08 22:59:53 +0000

    fixed errors

--- calcarea.py
+++ calcarea.py
@@ -1,6 +1,6 @@
 def calc_area(base, height):
     # calculate area
-
+   
     # process
     area = base * height / 2
     # output
@@ -12,10 +12,13 @@ def main():
 
     # input
     try:
+        base = 0
+        height = 0
         base = float(input("What is the base of the triangle: "))
         height = float(input("What is the height of the triangle: "))
     except ValueError:
         print("Input should be float")
+
 
     # call functions
     calc_area(base, height)
